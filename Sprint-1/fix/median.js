785761d237d2588d227248b018237ba4abdce645
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-07-19 22:09:24 +0100

    Fixing the calculate mean function to pass all the test

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,31 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
+  if (!Array.isArray(list)) {
+    return null;
+  }
+
+  const numericValues = list.filter(
+    (item) => typeof item === "number" && Number.isFinite(item)
+  );
+
+  if (numericValues.length === 0) {
+    return null;
+  }
+
+  let median = 0;
+  const sortedList = [...numericValues].sort(compareNumbers);
+  const middleIndex = Math.floor(sortedList.length / 2);
+  if (sortedList.length % 2 === 0) {
+    median = (sortedList[middleIndex - 1] + sortedList[middleIndex]) / 2;
+  } else {
+    median = sortedList[middleIndex];
+  }
   return median;
+}
+
+function compareNumbers(a, b) {
+  return a - b;
 }
 
 module.exports = calculateMedian;
